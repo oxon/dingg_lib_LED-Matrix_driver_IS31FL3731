101223d35f4d180b32d8ad4222c6fffbd7193e37
Jascha Haldemann <jh@oxon.ch>
2017-02-01 13:05:56 +0100

    init

---
+++ IS31FL3731.cpp
@@ -0,0 +1,286 @@
+/**-----------------------------------------------------------------------------
+ * \file    IS31FL3731.cpp
+ * \author  jh
+ * \date    xx.01.2017
+ * @{
+ -----------------------------------------------------------------------------*/
+
+ /* Includes --------------------------------------------------- */
+#include "IS31FL3731.h"
+
+/*
+ * Recommended workflow:
+ * Power on -> enableHW() -> configure register -> enableSW() -> display something ->
+ * disableSW() (matrix blanked) -> read and/or clear register -> disableHW()
+ */
+
+/* Public ----------------------------------------------------- */
+void IS31FL3731::begin(volatile uint8_t *enPort, uint8_t enPin, uint8_t i2cAddr)
+{
+  i2cAddr_ = i2cAddr;
+  enPort_ = enPort;
+  enPin_ = enPin;
+
+  ISSIWire_.begin();
+
+  enableHW();
+
+  /* init all LEDs of all frames */  
+  for (uint8_t f = FRAME1; f <= FRAME8; f++) 
+  {
+    selectFrame(f);
+
+    // //TODO: allow Variable LED-Matrix sizes
+    // // LEDs of Matrix A
+    // for (uint8_t i = REG_CONTROL_LED_A_START; i <= REG_CONTROL_LED_A_END; i += 0x02)
+    // {
+    //   if (i < y_*2)
+    //   {
+    //     if (x_ >= 8) writeRegister(i, 0xFF);
+    //     else ...
+    //   }
+    // }
+
+    //blup: only 8x8 for now
+    // LEDs of Matrix A
+    for (uint8_t i = REG_CONTROL_LED_A_START; i <= REG_CONTROL_LED_A_END; i += 0x02)
+    {
+      if (i == 0x10) writeRegister(i, 0x00);  // Column      9: off
+      else writeRegister(i, 0xFF);            // Column 1... 8: on
+    }
+    // LEDs of Matrix B
+    for (uint8_t i = REG_CONTROL_LED_B_START; i <= REG_CONTROL_LED_B_END; i += 0x02) writeRegister(i, 0x00); // Column 1... 9: off
+
+    // All Blink off
+    for (uint8_t i = REG_CONTROL_BLINK_START; i <= REG_CONTROL_BLINK_END; i += 0x01) writeRegister(i, 0x00);
+
+    // All PWM dc = 0
+    for (uint8_t i = REG_PWM_START; i <= REG_PWM_END; i += 0x01) writeRegister(i, 0x00);
+  }  
+
+  /* init */
+  setMode(PICTURE_MODE);
+  setAudioSync(false);
+  setFrame(FRAME1);
+
+  enableSW();
+}
+
+void IS31FL3731::end()
+{
+  disableHW();
+  ISSIWire_.end();
+}
+
+void IS31FL3731::enableHW()
+{
+  *enPort_ |= (1 << enPin_);          // HW enable (enable r/w)
+  delay(100);
+  disableSW();
+}
+
+void IS31FL3731::enableSW()
+{
+  selectFrame(FUNCTION_REGISTERS);
+  writeRegister(REG_SHUTDOWN, 0x01);  // SW enable (normal mode)
+}
+
+void IS31FL3731::disableHW()
+{
+  clear();
+  disableSW();
+  *enPort_ &= ~(1 << enPin_);         // HW shutdown (disable r/w)
+}
+
+void IS31FL3731::disableSW()
+{
+  selectFrame(FUNCTION_REGISTERS);
+  writeRegister(REG_SHUTDOWN, 0x00);  // SW shutdown (shutdown mode)
+}
+
+void IS31FL3731::setFrame(frame frameNr)
+{
+  if (frameNr > FRAME8) frameNr = FRAME1;
+  frame_ = frameNr;
+}
+
+void IS31FL3731::setMode(mode modeNr)
+{
+  selectFrame(FUNCTION_REGISTERS);
+  uint8_t conf = readRegister(REG_CONF);
+  conf = (conf & 0b11100111) | (modeNr << 3);
+  writeRegister(REG_CONF, conf);
+}
+
+void IS31FL3731::setDisplayOptions(bool intensityControl, bool blinkEnable, uint8_t blinkPeriodTime)  // in ms
+{
+  uint8_t displayOption = (intensityControl << 5) | (blinkEnable << 3) | blinkPeriodTime;
+  selectFrame(FUNCTION_REGISTERS);
+  writeRegister(REG_DISPLAY_OPTION, displayOption);
+}
+
+// void IS31FL3731::drawPixel(uint8_t x, uint8_t y, uint8_t brigtness)
+// {
+//   if ((x >= x_) || (y >= y_)) return;
+
+//   selectFrame(frame_);
+//   writeRegister(REG_PWM_START + x + y*MAX_NUMBER_OF_COLUMNS, brigtness);
+// }
+
+void IS31FL3731::drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+  switch (getRotation()) {
+  case 1:
+    _swap_int16_t(x, y);
+    x = 16 - x - 1;
+    break;
+  case 2:
+    x = 16 - x - 1;
+    y = 9 - y - 1;
+    break;
+  case 3:
+    _swap_int16_t(x, y);
+    y = 9 - y - 1;
+    break;
+  }
+
+  if ((x >= x_) || (y >= y_)) return;
+  if (color > 255) color = 255;
+
+  selectFrame(frame_);
+  writeRegister(REG_PWM_START + x + y*MAX_NUMBER_OF_COLUMNS, color);
+  return;
+}
+
+void IS31FL3731::clear()
+{
+  selectFrame(frame_);
+
+  /* clear row wise (since all 144 at once didn't work) */
+  for (uint8_t y = 0; y < MAX_NUMBER_OF_ROWS; y++)
+  {
+    ISSIWire_.beginTransmission(i2cAddr_);
+    ISSIWire_.write(REG_PWM_START + y*MAX_NUMBER_OF_COLUMNS);
+    for (uint8_t x = 0; x < MAX_NUMBER_OF_COLUMNS; x++) ISSIWire_.write(0x00);  // write 0 for each LED
+    ISSIWire_.endTransmission();
+  }
+}
+
+/* Picture Mode */
+void IS31FL3731::displayFrame(frame frameNr)
+{
+  if (frameNr > FRAME8) frameNr = FRAME1;
+  selectFrame(FUNCTION_REGISTERS);
+  writeRegister(REG_PICTURE_DISPLAY, frameNr);
+}
+
+void IS31FL3731::setFadeOutTime(uint16_t time)  // in ms
+{
+  //TODO: FOT = 26ms * 2^time
+  //time /= TYP_FADE_OUT_TIME;
+  //selectFrame(FUNCTION_REGISTERS);
+  //writeRegister(REG_CONTROL_BREATH1, );
+}
+
+void IS31FL3731::setFadeInTime(uint16_t time)  // in ms
+{
+  //TODO: FIT = 26ms * 2^time
+  //time /= TYP_FADE_IN_TIME;
+  //selectFrame(FUNCTION_REGISTERS);
+  //writeRegister(REG_CONTROL_BREATH1, );
+}
+
+void IS31FL3731::setBreathControl(bool enable, uint8_t extinguishTime)
+{
+  selectFrame(FUNCTION_REGISTERS);
+  writeRegister(REG_CONTROL_BREATH2, ((enable << 4) | extinguishTime));
+}
+
+void IS31FL3731::setAudioSync(bool enable)  // enable to modulate the intensity of the matrix by the audio input signal
+{
+  selectFrame(FUNCTION_REGISTERS);
+  writeRegister(REG_AUDIO_SYNC, enable);
+}
+
+/* Auto Frame Play Mode */
+void IS31FL3731::setStartFrame(frame frameNr)
+{
+  selectFrame(FUNCTION_REGISTERS);
+  uint8_t conf = readRegister(REG_CONF);
+  conf = (conf & 0b11111000) | frameNr;
+  writeRegister(REG_CONF, conf);
+}
+
+void IS31FL3731::setNumberOfLoops(numberOfLoops loop)
+{
+  uint8_t controlAutoPlay1 = readRegister(REG_CONTROL_AUTO_PLAY1);
+  controlAutoPlay1 = (controlAutoPlay1 & 0b10001111) | (loop << 4);
+  writeRegister(REG_CONTROL_AUTO_PLAY1, controlAutoPlay1);
+}
+
+void IS31FL3731::setNumberOfFrames(numberOfFrames frame)
+{
+  uint8_t controlAutoPlay1 = readRegister(REG_CONTROL_AUTO_PLAY1);
+  controlAutoPlay1 = (controlAutoPlay1 & 0b11111000) | frame;
+  writeRegister(REG_CONTROL_AUTO_PLAY1, controlAutoPlay1);
+}
+
+void IS31FL3731::setDelayBetweenFrames(uint16_t delay)  // in ms
+{
+  delay /= TYP_FRAME_DELAY_TIME;
+  writeRegister(REG_CONTROL_AUTO_PLAY2, (delay & 0x3F));
+}
+
+bool IS31FL3731::getMovieFinished()
+{
+  return (readRegister(REG_FRAME_STATE) & 0b00010000);
+}
+
+uint8_t IS31FL3731::getCurrentFrameDisplay()
+{
+  return frame(readRegister(REG_FRAME_STATE) & 0b00000111);
+}
+
+/* Audio Frame Play Mode */
+void IS31FL3731::setAudioADCRate(uint8_t rate)
+{
+  writeRegister(REG_AUDIO_ADC_RATE, rate);
+}
+
+void IS31FL3731::setAGCControl(bool mode, bool enable, uint8_t gain)
+{
+  writeRegister(REG_CONTROL_AGC, ((mode << 4) | (enable << 3) | gain));
+}
+
+/* Private ---------------------------------------------------- */
+void IS31FL3731::selectFrame(frame frameNr)
+{
+  // Serial.print("selectFrame = "); Serial.println(frameNr);
+
+  ISSIWire_.beginTransmission(i2cAddr_);
+  ISSIWire_.write(REG_CMD);
+  ISSIWire_.write(frameNr);
+  ISSIWire_.endTransmission();
+
+  if (frameNr <= FRAME8) frame_ = frameNr;  // set current frame
+}
+
+uint8_t IS31FL3731::readRegister(uint8_t address)
+{
+  ISSIWire_.beginTransmission(i2cAddr_);
+  ISSIWire_.write(address);
+  ISSIWire_.endTransmission();
+
+  ISSIWire_.requestFrom(i2cAddr_, uint8_t(1));
+  return ISSIWire_.read();
+}
+
+void IS31FL3731::writeRegister(uint8_t address, uint8_t value)
+{
+  // Serial.print("writeRegister 0x"); Serial.print(address, HEX);
+  // Serial.print(" = "); Serial.println(value, HEX);
+  ISSIWire_.beginTransmission(i2cAddr_);
+  ISSIWire_.write(address);
+  ISSIWire_.write(value);
+  ISSIWire_.endTransmission();
+}
---
+++ IS31FL3731.h
@@ -0,0 +1,157 @@
+/**-----------------------------------------------------------------------------
+ * \file    IS31FL3731.h
+ * \author  jh
+ * \date    xx.01.2017
+ *
+ * \version 1.0
+ *
+ * \brief   The IS31FL3731 can drive a 16x9 (144) LED-Matrix.
+ *          It is able to control the brightness of every LED individually by
+ *          setting a 8-bit PWM duty cycle.
+ *          It consists of 8 frames that you can prepare beforehand and display
+ *          with either the displayFrame()-function (Picture Mode) or in a loop
+ *          (Auto Frame Play Mode).
+ * 
+ * \note:   The IS31FL3731 is very critical when it comes to high temperatures.
+ *          Do not exceed 260°C (for > 30s) in the production process
+ *
+ * @{
+ -----------------------------------------------------------------------------*/
+
+/* Define to prevent recursive inclusion -----------------------*/
+#ifndef IS31FL3731_H_
+#define IS31Fl3731_H_
+
+/* Includes --------------------------------------------------- */
+#include <Arduino.h>
+#include <Wire.h>
+//#include <LEDMatrix.h>
+#include <Adafruit_GFX.h>
+
+/* Typedefs ----------------------------------------------------*/
+
+/* Macros ----------------------------------------------------- */
+#ifndef _swap_int16_t
+  #define _swap_int16_t(a, b) {int16_t temp = a; a = b; b = temp;}
+#endif
+
+/* Defines -----------------------------------------------------*/
+
+/* Class ------------------------------------------------------ */
+//class IS31FL3731
+class IS31FL3731 : public Adafruit_GFX
+{
+public:
+  /* constructor(s) & deconstructor */
+  //IS31FL3731(TwoWire& ISSIWire, uint8_t x = 9, uint8_t y = 16) : ISSIWire_(ISSIWire), x_(x), y_(y) {};
+  IS31FL3731(TwoWire& ISSIWire, uint16_t x = 9, uint16_t y = 16) : ISSIWire_(ISSIWire), Adafruit_GFX(x, y) {};
+  ~IS31FL3731() {};
+
+  /* public constants (static) */
+  // ...
+
+  /* public enumerations */
+  enum mode : uint8_t {PICTURE_MODE = 0, AUTO_FRAME_PLAY_MODE = 1, AUDIO_FRAME_PLAY_MODE = 2};
+  enum frame : uint8_t {FRAME1 = 0, FRAME2 = 1, FRAME3 = 2, FRAME4 = 3, FRAME5 = 4, FRAME6 = 5, FRAME7 = 6, FRAME8 = 7, FUNCTION_REGISTERS = 8};
+  enum numberOfLoops : uint8_t {ENDLESS = 0, LOOP1 = 1, LOOP2 = 2, LOOP3 = 3, LOOP4 = 4, LOOP5 = 5, LOOP6 = 6, LOOP7 = 7};
+  enum numberOfFrames : uint8_t {ALL_FRAMES = 0, ONE_FRAME = 1, TWO_FRAMES = 2, THREE_FRAMES = 3, FOUR_FRAMES = 4, FIVE_FRAMES = 5, SIX_FRAMES = 6, SEVEN_FRAMES = 7};
+
+  /* public methods */
+  void begin(volatile uint8_t *enPort, uint8_t enPin, uint8_t i2cAddr = DEFAULT_I2C_ADDR);  //Example: Matrix.begin(&PORTB, 3);
+  void end();
+  void enableHW();
+  void enableSW();
+  void disableHW();
+  void disableSW();
+  void setFrame(frame frameNr);
+  void setMode(mode modeNr);
+  void setDisplayOptions(bool intensityControl, bool blinkEnable, uint8_t blinkPeriodTime);  // in ms
+  //void drawPixel(uint8_t x, uint8_t y, uint8_t brigtness);
+  void drawPixel(int16_t x, int16_t y, uint16_t color);
+  void clear();
+  
+  // Picture Mode methods:
+  void displayFrame(frame frameNr);
+  void setFadeOutTime(uint16_t time);  // in ms
+  void setFadeInTime(uint16_t time);   // in ms
+  void setBreathControl(bool enable, uint8_t extinguishTime);
+  void setAudioSync(bool enable);
+  
+  // Auto Frame Play Mode methods:
+  void setStartFrame(frame frameNr);
+  void setNumberOfLoops(numberOfLoops loop);
+  void setNumberOfFrames(numberOfFrames frame);
+  void setDelayBetweenFrames(uint16_t delay);  // in ms
+  bool getMovieFinished();
+  uint8_t getCurrentFrameDisplay();
+  
+  // Audio Frame Play Mode methods:
+  void setAudioADCRate(uint8_t rate);
+  void setAGCControl(bool mode, bool enable, uint8_t gain);
+
+/* 
+ * Note:
+ * Frame Registers can only be read in software shutdown mode as SDB pin is high.
+ * The Function Register can be read in software shutdown mode or operating mode.
+ */
+
+private:
+  /* attributes */
+  TwoWire& ISSIWire_;
+  uint8_t x_;
+  uint8_t y_;
+
+  /* private constants (static) */
+  static const uint8_t DEFAULT_I2C_ADDR         = 0x74;  // AD connected to GND, 0x75 (VCC), 0x76 (SCL), 0x77 (SDA)
+
+  // Command Register:
+  static const uint8_t REG_CMD                  = 0xFD;  // Command Register to point at the current frame
+
+  // Frame Registers: (select FRAME1... FRAME8)
+  static const uint8_t REG_CONTROL_LED_A_START  = 0x00;  // 0x00, 0x02, 0x04... 0x10
+  static const uint8_t REG_CONTROL_LED_A_END    = 0x10;
+  static const uint8_t REG_CONTROL_LED_B_START  = 0x01;  // 0x01, 0x03, 0x05... 0x11
+  static const uint8_t REG_CONTROL_LED_B_END    = 0x11;
+  static const uint8_t REG_CONTROL_BLINK_START  = 0x12;  // 0x12... 0x23
+  static const uint8_t REG_CONTROL_BLINK_END    = 0x23;
+  static const uint8_t REG_PWM_START            = 0x24;  // 0x24... 0xB3
+  static const uint8_t REG_PWM_END              = 0xB3;
+
+  // Function Registers: (before writing or to a Function Register, you have to select "frame nine" -> FUNCTION_REGISTERS)
+  static const uint8_t REG_CONF                 = 0x00;
+  static const uint8_t REG_PICTURE_DISPLAY      = 0x01;
+  static const uint8_t REG_CONTROL_AUTO_PLAY1   = 0x02;
+  static const uint8_t REG_CONTROL_AUTO_PLAY2   = 0x03;
+  static const uint8_t REG_DISPLAY_OPTION       = 0x05;
+  static const uint8_t REG_AUDIO_SYNC           = 0x06;
+  static const uint8_t REG_FRAME_STATE          = 0x07;
+  static const uint8_t REG_CONTROL_BREATH1      = 0x08;
+  static const uint8_t REG_CONTROL_BREATH2      = 0x09;
+  static const uint8_t REG_SHUTDOWN             = 0x0A;
+  static const uint8_t REG_CONTROL_AGC          = 0x0B;
+  static const uint8_t REG_AUDIO_ADC_RATE       = 0x0C;
+
+  // constants
+  static const uint8_t TYP_FRAME_DELAY_TIME     = 11;     // in ms
+  static const uint8_t TYP_FADE_OUT_TIME        = 26;     // in ms
+  static const uint8_t TYP_FADE_IN_TIME         = 26;     // in ms
+  static const uint8_t MAX_NUMBER_OF_COLUMNS    = 16;
+  static const uint8_t MAX_NUMBER_OF_ROWS       = 9;
+
+  /* private variables */
+  uint8_t i2cAddr_;
+  volatile uint8_t *enPort_;
+  uint8_t enPin_;
+  frame frame_;
+
+  /* private methods */
+  void selectFrame(frame frameNr);
+  uint8_t readRegister(uint8_t address);
+  void writeRegister(uint8_t address, uint8_t value);
+};
+
+#endif
+
+/**
+ * @}
+ */
