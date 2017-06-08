f86f33e4b1d6e1d18983e6f5893d65b574e41047
Jascha Haldemann <jh@oxon.ch>
2017-06-08 08:24:15 +0200

    added LGPL-V2.1 license

--- IS31FL3731.cpp
+++ IS31FL3731.cpp
@@ -12,6 +12,18 @@
 * Power on -> enableHW() -> configure register -> enableSW() -> display something ->
 * disableSW() (matrix blanked) -> read and/or clear register -> disableHW()
 *
+* \license LGPL-V2.1
+* Copyright (c) 2017 OXON AG. All rights reserved.
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, see 'http://www.gnu.org/licenses/'
 *******************************************************************************/
 
 /* ================================= Imports ================================ */
--- IS31FL3731.h
+++ IS31FL3731.h
@@ -19,6 +19,18 @@
 * \note:   The IS31FL3731 is very critical when it comes to high temperatures.
 *          Do not exceed 260°C (for > 30s) in the production process.
 *
+* \license LGPL-V2.1
+* Copyright (c) 2017 OXON AG. All rights reserved.
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, see 'http://www.gnu.org/licenses/'
 ********************************************************************************
 * LED-Matrix driver Library
 *******************************************************************************/
